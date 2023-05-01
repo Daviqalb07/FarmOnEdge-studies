344e439b713bec8640ee081914e0c32b5b56ce12
Davi Queiroz <daviqueiroz2002.dq@gmail.com>
2023-05-01 19:23:15 -0300

    Separando pastas para servidor e para cliente

--- PiCameraClient/PiCameraClient.py
+++ PiCameraClient/PiCameraClient.py
@@ -1,7 +1,8 @@
 import os
 import paho.mqtt.client as mqtt
-from io import BytesIO
 from picamera import PiCamera
+from io import BytesIO
+from base64 import b64encode
 from time import sleep
 from dotenv import load_dotenv
 
@@ -14,45 +15,46 @@ IMAGES_TOPIC = 'images'
 CAMERA_RESOLUTION = (1080, 720)
 CAPTURE_PERIOD = 10 # in seconds
 
-def start_camera(resolution: tuple, exposure_mode:str='auto') -> PiCamera:
-    camera = PiCamera()
+
+def capture(camera: PiCamera, image_stream: BytesIO, resolution: tuple, format='jpeg', exposure_mode='auto', vflip=False):
     camera.resolution = resolution
     camera.exposure_mode = exposure_mode
-    return camera
-
-def close_camera(camera: PiCamera):
-    camera.close()
-
-def capture(camera: PiCamera, image_stream: BytesIO, format='jpeg'):
+    camera.vflip = vflip
     camera.capture(image_stream, format= format)
+
 
 def read_image(image_stream: BytesIO) -> bytes:
     image_stream.seek(0)
     return image_stream.read()
 
+
 def main():
     client = mqtt.Client('raspberrypi')
     client.connect(BROKER_IP, BROKER_PORT)
 
-    camera = start_camera(CAMERA_RESOLUTION, exposure_mode='off')
+    camera = PiCamera()
     
-
     while True:
+        client.loop()
         try:
             image_stream = BytesIO()
-            
-            capture(camera, image_stream)
+
+            capture(camera, image_stream, CAMERA_RESOLUTION, vflip=True)
             
             image = read_image(image_stream)
             image_stream.close()
-            client.publish(IMAGES_TOPIC, image)
+
+            data = b64encode(image)
+
+            client.publish(IMAGES_TOPIC, data)
 
             sleep(CAPTURE_PERIOD)
         except:
-            close_camera(camera)
+            camera.close()
             break
 
         finally:
             image_stream.close()
+
 
 main()
--- PiCameraServer/PiCameraServer.py
+++ PiCameraServer/PiCameraServer.py
@@ -1,6 +1,7 @@
 import os
 import paho.mqtt.client as mqtt
 from io import BytesIO
+from base64 import b64decode
 from PIL import Image
 from dotenv import load_dotenv
 
@@ -10,18 +11,22 @@ BROKER_IP = os.environ['BROKER_IP']
 BROKER_PORT = 1883
 IMAGES_TOPIC = 'images'
 
+
 def main():
     client = mqtt.Client('server')
     client.connect(BROKER_IP, BROKER_PORT)
-    
+
     client.on_message = on_message
     client.subscribe(IMAGES_TOPIC)
 
     client.loop_forever()
 
+
 def on_message(client, userdata, message):
-    image_bytes = message.payload
-    image = Image.open(BytesIO(image_bytes))
+    data = b64decode(message.payload)
+    
+    image = Image.open(BytesIO(data))
     image.show()
+
 
 main()
